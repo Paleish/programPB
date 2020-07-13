e79ca951de956843fbf7c0605c77e2529ef98329
lrsxnn <lrs-1@163.com>
2020-07-13 11:39:12 +0800

    添加自动生成C#的proto脚本

--- LockstepSrvPB/makeClientProto.bat
+++ LockstepSrvPB/makeClientProto.bat
@@ -2,6 +2,6 @@ for %%i in (*.proto) do (
     protoc.exe --cpp_out=../Client_CPP %%i
 )
 for %%i in (*.proto) do (
-    protoc.exe --csharp_out=../Client_CSHARP %%i
+    protoc.exe --csharp_out=../Client_CSHARP/pb %%i
 )
 pause
